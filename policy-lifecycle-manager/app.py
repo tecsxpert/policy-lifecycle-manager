61c2f4db8fa6fd72d12d4a3619c25c0ee13faadf
Rajatha2511 <rajathavinyas16@gmail.com>
2026-04-21 16:32:27 +0530

    Add Flask project files to repo root

---
+++ policy-lifecycle-manager/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from routes.main_routes import main
+app=Flask(__name__)
+app.register_blueprint(main)
+
+@app.route('/')
+def home():
+    return "Flask is running!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
